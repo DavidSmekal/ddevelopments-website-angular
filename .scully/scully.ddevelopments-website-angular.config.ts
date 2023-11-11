3b390ef81c64350779c6c1b3f265e48a6e1b3394
David Smekal <David.Smekal@hotmail.com>
2023-11-11 13:01:58 -0700

    try something

--- .scully/scully.ddevelopments-website-angular.config.ts
+++ .scully/scully.ddevelopments-website-angular.config.ts
@@ -3,7 +3,7 @@ import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'demo',
-  distFolder: './dist/ddevelopments-website-angular', // output directory of your Angular build artifacts
+  distFolder: './dist/davidsmekal.github.io', // output directory of your Angular build artifacts
   outDir: './dist/static', // directory for scully build artifacts
   defaultPostRenderers: [],
   routes: {},
